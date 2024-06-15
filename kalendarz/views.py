5580249e2532e0ab314e6834eb9357e85793759c
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-06-15 18:02:46 +0200

    add click on date option

--- kalendarz/views.py
+++ kalendarz/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views import View
+
 from .models import Event
 from .forms import EventForm
+from django.middleware.csrf import get_token
 
 
 class CalendarView(View):
@@ -13,7 +15,13 @@ class CalendarView(View):
 
 class AddEventView(LoginRequiredMixin, View):
     def get(self, request):
-        form = EventForm()
+        initial_data = {}
+        date = request.GET.get('date')
+        if date:
+            initial_data['start_time'] = f"{date}T18:00"
+            initial_data['end_time'] = f"{date}T19:00"
+
+        form = EventForm(initial=initial_data)
         return render(request, 'kalendarz/add_event.html', {'form': form})
 
     def post(self, request):
@@ -24,3 +32,5 @@ class AddEventView(LoginRequiredMixin, View):
             event.save()
             return redirect('calendar')
         return render(request, 'kalendarz/add_event.html', {'form': form})
+
+
